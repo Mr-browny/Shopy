13061e3b3728e55bbed5e5b2c074ddda60aac574
George Ikwegbu <g.ikwegbu@gmail.com>
2021-10-20 02:03:09 +0100

    Added The SplashScreen
    1. Used a package https://pub.dev/packages/splash_screen_view, to add a splash screen capability for the app, worked like a charm.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopy/routes.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
 
 // Importing Screens
 import 'screens/splash/splash_screen.dart';
@@ -11,11 +12,30 @@ class ShopyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Widget splash = SplashScreenView(
+      navigateRoute: SplashScreen(),
+      duration: 5000,
+      imageSize: 130,
+      imageSrc: "./assets/images/icon.png",
+      text: "Shopy",
+      textType: TextType.ColorizeAnimationText,
+      textStyle: TextStyle(
+        fontSize: 40.0,
+      ),
+      colors: [
+        Colors.purple,
+        Colors.blue,
+        Colors.yellow,
+        Colors.red,
+      ],
+      backgroundColor: Colors.white,
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Shopy',
       theme: themeData(),
-      initialRoute: SplashScreen.routeName,
+      home: splash,
+      // initialRoute: SplashScreen.routeName,
       routes: routes,
     );
   }
